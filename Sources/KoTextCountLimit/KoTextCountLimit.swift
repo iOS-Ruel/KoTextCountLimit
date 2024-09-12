a23e9117f40c036ea08c9c2d317a973f5dacc686
Ruel <c0407h@gmail.com>
2024-09-12 13:48:56 +0900

    issue: issue #5 Correct the problem where consonants are entered when limiting the number of characters
    1. Correct the problem where consonants are entered when limiting the number of characters
    2. Adding a Return of Characters Method

--- Sources/KoTextCountLimit/KoTextCountLimit.swift
+++ Sources/KoTextCountLimit/KoTextCountLimit.swift
@@ -6,10 +6,13 @@ import UIKit
 public class KoTextCountLimit {
     public init() {}
     
+    private var textCount: Int = 0
+    
     public func shouldChangeText(for textInput: UITextInput,
                                  in range: NSRange,
                                  replacementText text: String,
                                  maxCharacterLimit: Int) -> Bool {
+        
         if text.isBackspace {
             return true
         }
@@ -25,21 +28,48 @@ public class KoTextCountLimit {
         return true
     }
     
-    private func canAddText(currentText: String, replacementText text: String, maxCharacterLimit: Int) -> Bool {
+    private func canAddText(currentText: String, 
+                            replacementText text: String,
+                            maxCharacterLimit: Int) -> Bool {
+        
         let hasPostPosition = currentText.hasPostPosition
         let isConsonantChar = text.isConsonant
         let isVowelChar = text.isVowel
+        let currentTextCount = currentText.utf16.count
+        
+        if currentTextCount >= maxCharacterLimit {
+
+            if let lastText = currentText.last {
+                if lastText.isVowel {
+                    return false
+                } else if lastText.isConsonant {
+                    return isVowelChar
+                } else {
+                    return isConsonantChar
+                }
+            }
+        }
         
         if !hasPostPosition && isConsonantChar {
-            return currentText.utf16.count + text.count <= maxCharacterLimit || !isVowelChar
-        } else if hasPostPosition && !isConsonantChar {
-            if let lastText = currentText.last, lastText.isConsonant {
-                return true
-            } else {
-                return currentText.count > maxCharacterLimit ? isVowelChar : false
-            }
-        } else {
-            return false
+            return currentTextCount < maxCharacterLimit
         }
+        
+        return currentTextCount + text.count <= maxCharacterLimit
+    }
+    
+    public func didChangeText(for textInput: UITextInput, 
+                              maxCharacterLimit: Int) {
+        guard let textView = textInput as? UITextView else { return  }
+        
+        if textView.text.count > maxCharacterLimit {
+            textView.text = String(textView.text.prefix(maxCharacterLimit))
+            self.textCount = maxCharacterLimit
+        }
+        
+        self.textCount = textView.text.count
+    }
+    
+    public func getTextCount() -> Int {
+        return self.textCount
     }
 }
